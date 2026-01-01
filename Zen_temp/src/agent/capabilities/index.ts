816135080a722044c722cf3fdaa5d641a7195d18
KhanhRomVN <khanhromvn@gmail.com>
2026-01-01 16:09:28 +0700

    update

--- Zen_temp/src/agent/capabilities/index.ts
+++
@@ -1,4 +0,0 @@
-export { FileReadCapability } from "./FileReadCapability";
-export { FileEditCapability } from "./FileEditCapability";
-export { FileAddCapability } from "./FileAddCapability";
-export { CommandExecutor } from "./CommandExecutor";
